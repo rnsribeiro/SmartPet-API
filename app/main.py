6e59e64c012b2a410d5463aed2e6ccb53415c5a6
Rodrigo Nunes <rodrigo.ribeiro@wconnect.com.br>
2024-08-29 18:59:53 -0300

    Create token with username and password

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from handlers.user import create_user, read_user, list_users
+from handlers.auth import login_for_access_token, get_current_user
 
 app = FastAPI()
 
-# Definição das rotas
+# Rota de login
+app.post("/token")(login_for_access_token)
+
+# Rotas protegidas
 app.post("/user/")(create_user)
 app.get("/user/{user_id}")(read_user)
 app.get("/user/")(list_users)
--- handlers/user.py
+++ handlers/user.py
@@ -1,13 +1,14 @@
-from fastapi import HTTPException
+from fastapi import HTTPException, Depends
 from pydantic import BaseModel
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 import bcrypt
+from handlers.auth import get_current_user
 
 # Conecte-se ao MongoDB
 client = MongoClient("mongodb://smartpet:smartpet@localhost:27017/")
-db = client.smartpet  # Substitua 'mydatabase' pelo nome do seu banco de dados
-users_collection = db.users  # Substitua 'users' pelo nome da coleção que você deseja usar
+db = client.smartpet
+users_collection = db.users
 
 # Modelo para a criação de um novo usuário
 class UserCreate(BaseModel):
@@ -18,14 +19,13 @@ class UserCreate(BaseModel):
 
 # Modelo para a resposta da API com o ID e informações do usuário
 class User(BaseModel):
-    id: str  # Usaremos o ID do MongoDB como uma string
+    id: str
     username: str
     name: str
     email: str
 
 # Handler para criar um novo usuário
 def create_user(user: UserCreate):
-    # Verifica se o username ou email já existem
     existing_user = users_collection.find_one({"$or": [{"username": user.username}, {"email": user.email}]})
     if existing_user:
         if existing_user.get("username") == user.username:
@@ -33,32 +33,26 @@ def create_user(user: UserCreate):
         if existing_user.get("email") == user.email:
             raise HTTPException(status_code=400, detail="Email already registered")
     
-    # Criptografa a senha
     hashed_password = bcrypt.hashpw(user.password.encode('utf-8'), bcrypt.gensalt())
-    
-    # Insere o novo usuário na base de dados
     new_user = {
         "username": user.username,
         "name": user.name,
         "email": user.email,
-        "password": hashed_password.decode('utf-8')  # Armazena a senha criptografada
+        "password": hashed_password.decode('utf-8')
     }
     result = users_collection.insert_one(new_user)
-    # Retorna o usuário criado, incluindo o ID gerado pelo MongoDB
     return User(id=str(result.inserted_id), username=user.username, name=user.name, email=user.email)
 
 # Handler para ler os dados de um usuário específico
-def read_user(user_id: str):
-    # Converte o ID para ObjectId
+def read_user(user_id: str, current_user: dict = Depends(get_current_user)):
     obj_id = ObjectId(user_id)
     user = users_collection.find_one({"_id": obj_id})
     if user is None:
         raise HTTPException(status_code=404, detail="User not found")
-    # Retorna o usuário encontrado (não retorna a senha)
     return User(id=str(user["_id"]), username=user["username"], name=user["name"], email=user["email"])
 
 # Handler para listar todos os usuários
-def list_users():
+def list_users(current_user: dict = Depends(get_current_user)):
     users = []
     for user in users_collection.find():
         users.append(User(id=str(user["_id"]), username=user["username"], name=user["name"], email=user["email"]))
